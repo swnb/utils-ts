3d54d35145e096a8780a9eab56a2ac8cabf7a9d7
async-rustacean <swnblovego@gmail.com>
2023-04-24 17:28:55 +0800

    update useCacheFn

--- src/callback/cache.ts
+++ src/callback/cache.ts
@@ -5,14 +5,14 @@ import { useCallback, useLayoutEffect, useRef } from 'react';
  * return unit callback
  */
 export function useCacheFn<Args extends any[], Ret extends any>(
-  callback: (...args: Args) => Ret
-) {
+  callback?: (...args: Args) => Ret
+): (...args: Args) => typeof callback extends undefined ? undefined : Ret {
   const cacheFn = useRef(callback);
   useLayoutEffect(() => {
     cacheFn.current = callback;
   });
 
   return useCallback((...args: Args) => {
-    return cacheFn.current(...args);
+    return cacheFn.current?.(...args) as any;
   }, []);
 }
